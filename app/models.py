cb5ed0a786a4bd89f0e6d227286572e4fdf9c313
Xavier Kibet <xavierkibet@gmail.com>
2018-06-29 11:27:06 +0300

    Add user class

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
-# File for models/classes
+from . import db
+
+class User(db.Model):
+	__tablename__='users'
+	id=db.Column(db.Integer,primary_key=True)
+	username=db.Column(db.String(255))
+	password=db.Column(db.String(255))
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
