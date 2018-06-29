ee8df0009c65cec42d90a5014dc5061182fd2a43
Xavier Kibet <xavierkibet@gmail.com>
2018-06-29 13:37:34 +0300

    implement working login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,22 @@
 from flask import render_template,request,redirect,url_for,abort
 from flask_login import login_user,login_required,current_user
+from ..models import User
 from .forms import LoginForm
 from . import auth
 
 # Views
-@auth.route('/login')
+@auth.route('/login', methods=["GET","POST"])
 def login():
     title = 'Login'
     Form = LoginForm()
-    return render_template('login.html', title = title ,Form=Form)
+    Error=False
+    if Form.validate_on_submit():
+        username=str(Form.username.data)
+        password=str(Form.username.data)
+        if username and password:
+            user=User.query.filter(User.username==username).first()
+            if user and user.verifypass(password):
+                login_user(user,Form.remember.data)
+                return redirect(request.args['next'])
+        Error=True
+    return render_template('login.html', title = title ,Form=Form,Error=Error)
--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ class User(db.Model):
 	id=db.Column(db.Integer,primary_key=True)
 	username=db.Column(db.String(255))
 	password=db.Column(db.String(255))
+	def verifypass(self,trial):
+		return self.password == trial
 	def save(self):
 		db.session.add(self)
 		db.session.commit()
