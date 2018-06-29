f8f9311347c2ea1074542127e44487a1d46bd27a
Xavier Kibet <xavierkibet@gmail.com>
2018-06-29 23:51:13 +0300

    Add migrations folder

---
+++ migrations/versions/7f113ecca609_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7f113ecca609
+Revises: 4cebc9a95907
+Create Date: 2018-06-29 12:33:56.939273
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7f113ecca609'
+down_revision = '4cebc9a95907'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
