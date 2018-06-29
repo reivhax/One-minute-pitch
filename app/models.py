afd88387a196bfd6ce4b64318465403c15170130
Xavier Kibet <xavierkibet@gmail.com>
2018-06-29 12:54:51 +0300

    update models

--- app/models.py
+++ app/models.py
@@ -11,27 +11,27 @@ class User(db.Model):
 class Post(db.Model):
 	__tablename__='posts'
 	id=db.Column(db.Integer,primary_key=True)
-    userid=db.Column(db.Integer)
-	text=db.Column(db.String(255))
+	userid=db.Column(db.Integer)
+	text=db.Column(db.String(65535))
 	def save(self):
 		db.session.add(self)
 		db.session.commit()
 class Comment(db.Model):
 	__tablename__='comments'
 	id=db.Column(db.Integer,primary_key=True)
-    userid=db.Column(db.Integer)
-    postid=db.Column(db.Integer)
+	userid=db.Column(db.Integer)
+	postid=db.Column(db.Integer)
 	text=db.Column(db.String(65535))
 	def save(self):
 		db.session.add(self)
 		db.session.commit()
 class Upvote(db.Model):
-    __tablename__='upvotes'
-    id=db.Column(db.Integer,primary_key=True)
-    userid=db.Column(db.Integer)
-    postid=db.Column(db.Integer)
+	__tablename__='upvotes'
+	id=db.Column(db.Integer,primary_key=True)
+	userid=db.Column(db.Integer)
+	postid=db.Column(db.Integer)
 class Downvote(db.Model):
-    __tablename__='downvotes'
-    id=db.Column(db.Integer,primary_key=True)
-    userid=db.Column(db.Integer)
-    postid=db.Column(db.Integer)
+	__tablename__='downvotes'
+	id=db.Column(db.Integer,primary_key=True)
+	userid=db.Column(db.Integer)
+	postid=db.Column(db.Integer)
