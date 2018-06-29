fa2b679feca01b71eb28185e413de04b526e62e8
Xavier Kibet <xavierkibet@gmail.com>
2018-06-29 11:26:53 +0300

    add sqlalchemy support

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 # from flask_login import LoginManager
 
 
@@ -12,12 +12,12 @@ from config import config_options
 # login_manager.session_protection = 'strong'
 # login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
 def create_app(config_name):
     '''
     Function that takes configuration setting key as an argument
-    
+
     Args:
         config_name : name of the configuration to be used
     '''
@@ -31,7 +31,7 @@ def create_app(config_name):
 
     # Initialising flask extensions
     bootstrap.init_app(app)
-    # db.init_app(app)
+    db.init_app(app)
     # login_manager.init_app(app)
 
     # Regestering the main blueprint
@@ -41,9 +41,5 @@ def create_app(config_name):
     # Regestering the auth bluprint
     # from .auth import auth as auth_blueprint
     # app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
-    # Setting config when using an API
-    # from .requests import configure_request
-    # configure_request(app)
 
     return app
