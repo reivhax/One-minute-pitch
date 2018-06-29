ab1d2fbe1a5d4b94e8d3d042e9be29c5425f467c
Xavier Kibet <xavierkibet@gmail.com>
2018-06-29 11:45:38 +0300

    add Post class

--- app/models.py
+++ app/models.py
@@ -8,3 +8,11 @@ class User(db.Model):
 	def save(self):
 		db.session.add(self)
 		db.session.commit()
+class Post(db.Model):
+	__tablename__='posts'
+	id=db.Column(db.Integer,primary_key=True)
+    userid=db.Column(db.Integer)
+	text=db.Column(db.String(255))
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
