0e76f09a43780c9b75c9e9f4c4c0263cb7428871
Xavier Kibet <xavierkibet@gmail.com>
2018-06-29 13:22:40 +0300

    add login form

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,10 @@
 from flask import render_template,request,redirect,url_for,abort
+from flask_login import login_user,login_required,current_user
 from . import main
 
 # Views
 @main.route('/')
+@login_required
 def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
     title = 'Home'
-
     return render_template('index.html', title = title )
-   
-
