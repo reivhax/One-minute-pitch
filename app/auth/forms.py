ff4e129431db77d03de2d4afbcea823ecdb296e3
Xavier Kibet <xavierkibet@gmail.com>
2018-07-01 22:45:50 +0300

    Switch to PasswordField From StringField

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,BooleanField
+from wtforms import StringField,SubmitField,BooleanField,PasswordField
 from wtforms.validators import Required
 
 class RegisterForm(FlaskForm):
 	username = StringField('username',validators=[Required()])
-	password = StringField('password', validators=[Required()])
+	password = PasswordField('password', validators=[Required()])
 	submit = SubmitField("submit")
 
 
 class LoginForm(FlaskForm):
 	username = StringField('username',validators=[Required()])
-	password = StringField('password', validators=[Required()])
+	password = PasswordField('password', validators=[Required()])
 	remember = BooleanField("Remember me")
 	submit = SubmitField("submit")
