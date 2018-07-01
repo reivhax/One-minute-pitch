5f577210da2d49209ca9cb28d468d738d6917772
Xavier Kibet <xavierkibet@gmail.com>
2018-07-01 22:37:45 +0300

    Update Database structure

---
+++ migrations/versions/edfc4b143cc8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: edfc4b143cc8
+Revises: 7f113ecca609
+Create Date: 2018-07-01 15:22:08.480876
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'edfc4b143cc8'
+down_revision = '7f113ecca609'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
