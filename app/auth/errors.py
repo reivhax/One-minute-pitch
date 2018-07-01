b56d6938d1614af9eb14522a3f98200bfbaa1bff
Xavier Kibet <xavierkibet@gmail.com>
2018-07-01 22:41:01 +0300

    Update 404 error page

--- app/auth/errors.py
+++ app/auth/errors.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import auth
+from ..models import Bloopers
 
 # Error handler decorator
 @auth.app_errorhandler(404)
@@ -8,4 +9,4 @@ def four_Ow_four(error):
     Function to render the 404 error page
     '''
     title = 'Not Found'
-    return render_template('fourOwfour.html', title=title),404
+    return render_template('fourOwfour.html', title=title, bloopers=Bloopers.getrandom()),404
--- app/main/errors.py
+++ app/main/errors.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from ..models import Bloopers
 
 # Error handler decorator
 @main.app_errorhandler(404)
@@ -8,4 +9,4 @@ def four_Ow_four(error):
     Function to render the 404 error page
     '''
     title = '404 page'
-    return render_template('fourOwfour.html', title=title),404
+    return render_template('fourOwfour.html', title=title,bloopers=Bloopers.getrandom()),404
