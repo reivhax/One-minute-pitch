675f8a8821d18f94e478a4e0c06210d430272374
Xavier Kibet <xavierkibet@gmail.com>
2018-07-01 22:50:52 +0300

    Implement Api endpiont access in javascript

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -20,21 +20,13 @@ $('document').ready(()=>{
       }
     )
   })
-  like=(pitch)=>{
-    $.ajax(
-      {
-        url:'/like/'+pitch,
-        method:'GET',
-        success:(data)=>{
-          if (data=='Success'){
-            l=$('#like'+pitch)[0].textContent
-            likes=parseInt(l)
-            likes++
-            $('#like'+pitch)[0].textContent=' '+likes.toString()+' '
-          }
-        }
-      }
-    )
+  submitcomment=(postid)=>{
+    $.post('/newcomment/'+postid, $('form#comment'+postid).serialize(),(data)=>{
+      $(data).hide().appendTo($('#comments'+postid)).show('fast');
+      count=$('#commentscount'+postid)
+      count.text(parseInt(count.text())+1)
+    });
+    [...$(".commentinput")].forEach(c=>c.value='')
   }
   like=(pitch)=>{
     $.ajax(
@@ -53,19 +45,50 @@ $('document').ready(()=>{
     )
   }
   dislike=(pitch)=>{
-    $.ajax(
-      {
-        url:'/dislike/'+pitch,
-        method:'GET',
-        success:(data)=>{
-          if (data=='Success'){
-            l=$('#dislike'+pitch)[0].textContent
-            dislikes=parseInt(l)
-            dislikes++
-            $('#dislike'+pitch)[0].textContent=' '+dislikes.toString()+' '
-          }
+    $.ajax({
+      url:'/dislike/'+pitch,
+      method:'GET',
+      success:(data)=>{
+        if (data=='Success'){
+          l=$('#dislike'+pitch)[0].textContent
+          dislikes=parseInt(l)
+          dislikes++
+          $('#dislike'+pitch)[0].textContent=' '+dislikes.toString()+' '
         }
       }
-    )
+    })
+  }
+  removecomment=(comment)=>{
+    $('#comment'+comment).hide(300,()=>{$('#comment'+comment).remove()});
+    count=$("#commentcount")
+    count.text(parseInt(count.text())-1)
+    $.post('/delete/comment/'+comment);
+  }
+  removepost=(post)=>{
+    $('#post'+post).hide(300,()=>{$('#post'+post).remove()});
+    count=$("#postcount")
+    count.text(parseInt(count.text())-1)
+    $.post('/delete/post/'+post);
+  }
+  fav=(post,elem)=>{
+    $.get('/fav/'+post);
+    elem=$(elem)
+    elem.toggleClass('glyphicon-heart')
+    elem.toggleClass('glyphicon-ok')
+    if(window.location.pathname=='/my-favorites/'){
+      elem.parent().hide(150)
+    }
+  }
+  recordchange=(ind,ls)=>{
+    if (ind==ls){
+      $("#manual").show('normal')
+      $("#manual").attr('required','')
+    }else{
+      $("#manual").hide('normal')
+      $("#manual").removeAttr('required')
+    }
+  }
+  filterelements=c=>{
+    $(".post").each((n,elem)=>{elem=$(elem);if(!elem.hasClass(c)){elem.hide('fast')}else{elem.show(50)}})
   }
 })
